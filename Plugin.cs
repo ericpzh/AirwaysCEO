4ead27f8f7ae02a77d8e7aa26d5eb14915d426dc
ericpzh <zp3@illinois.edu>
2024-08-09 09:55:49 -0700

    direct purchase

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using DG.Tweening;
 using HarmonyLib;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using TMPro;
 using UnityEngine;
@@ -33,7 +36,7 @@ namespace MoneyAirways
                 if (esc_button != null)
                 {
                     manager = esc_button.gameObject.AddComponent<Manager>();
-                    manager.cash = 0;
+                    manager.cash = 1000;
                 }
             }
         }
@@ -59,15 +62,9 @@ namespace MoneyAirways
             cashDisplayObj.transform.position = new Vector3(escButtonBottomRight.x + 0.75f, escButtonBottomRight.y + 0.3f, 0f);
             cashDisplay.text = "$ " + cash;
 
-            if (!UpgradeManager.Instance.UpgradeComplete && Input.GetKeyDown(KeyCode.R))
-            {
-                RefreshUpgrade();
-            }
-
-            if (UpgradeManager.Instance.UpgradeComplete && Input.GetKeyDown(KeyCode.B))
-            {
-                BuyUpgrade();
-            }
+            RefreshUpgrade();
+            BuyUpgrade();
+            BuyUpgrades();
         }
 
         private void InitTMP()
@@ -82,9 +79,94 @@ namespace MoneyAirways
             cashDisplay.rectTransform.sizeDelta = new Vector2(2, 1);
         }
 
+        private IEnumerator TurnFasterCoroutine()
+        {
+            float num = 0.1f;
+            Aircraft.TurnSpeed += num * Aircraft.TurnSpeed;
+            yield break;
+        }
+
+        private IEnumerator IncreaseAirspaceCoroutine()
+        {
+            float num = 0.5f;
+            float orthographicSize = Camera.main.orthographicSize;
+            Camera.main.DOOrthoSize(orthographicSize + num, 0.5f).SetUpdate(isIndependentUpdate: true);
+            LevelManager.Instance.MaximumCameraSizeByInbound += num;
+            yield break;
+        }
+
         private void BuyUpgrade()
         {
-            if (cash < buyCost || !UpgradeManager.Instance.UpgradeComplete)
+            if (!Input.GetKey(KeyCode.LeftShift) || upgrading)
+            {
+                return;
+            }
+
+            UpgradeOpt upgradeOpt = UpgradeOpt.NONE;
+            if (Input.GetKey(KeyCode.Alpha1) && cash >= buyCosts[1])
+            {
+                upgradeOpt = UpgradeOpt.LONGER_TAXIWAY;
+                TakeoffTaskManager.Instance.AddApron();
+            }
+            else if (Input.GetKey(KeyCode.Alpha2) && cash >= buyCosts[2])
+            {
+                upgradeOpt = UpgradeOpt.MORE_TAXIWAY_EXIT;
+                TakeoffTaskManager.Instance.AddEntrance();
+            }
+            else if (Input.GetKey(KeyCode.Alpha3) && cash >= buyCosts[3])
+            {
+                upgradeOpt = UpgradeOpt.TURN_FASTER;
+                StartCoroutine(TurnFasterCoroutine());
+            }
+            else if (Camera.main.orthographicSize <= LevelManager.Instance.maximumCameraOrthographicSize - 0.5f && Input.GetKey(KeyCode.Alpha4) && cash >= buyCosts[4])
+            {
+                upgradeOpt = UpgradeOpt.AIRSPACE;
+                StartCoroutine(IncreaseAirspaceCoroutine());
+            }
+            else if (RestrictedAreaManager.Instance != null && RestrictedAreaManager.Instance.counter < 3 && Input.GetKey(KeyCode.Alpha5) && cash >= buyCosts[5])
+            {
+                upgradeOpt = UpgradeOpt.COMPENSATION;
+                RestrictedAreaManager.Instance.ResetCounter();
+            }
+            else if (Input.GetKey(KeyCode.Alpha6) && cash >= buyCosts[6])
+            {
+                upgradeOpt = UpgradeOpt.NAVIGATION_WAYPOINT;
+                WaypointPropsManager.Instance.SpawnWaypointAutoHeading();
+            }
+            else if (Input.GetKey(KeyCode.Alpha7) && cash >= buyCosts[7])
+            {
+                upgradeOpt = UpgradeOpt.HOLD_WAYPOINT;
+                WaypointPropsManager.Instance.SpawnWaypointAutoHover();
+            }
+            else if (Input.GetKey(KeyCode.Alpha8) && cash >= buyCosts[8])
+            {
+                upgradeOpt = UpgradeOpt.LAND_WAYPOINT;
+                WaypointPropsManager.Instance.SpawnWaypointAutoLanding();
+            }
+            else if (Input.GetKey(KeyCode.Alpha9) && cash >= buyCosts[9])
+            {
+                upgradeOpt = UpgradeOpt.TAKEOFF_WAYPOINT;
+                WaypointPropsManager.Instance.SpawnWaypointTakingOff();
+            }
+
+            if (upgradeOpt != UpgradeOpt.NONE)
+            {
+                cash -= buyCosts[(int)upgradeOpt];
+                upgrading = true;
+                StartCoroutine(ResetUpgrading());
+            }
+            // UpgradeManager.Instance.counter[(int)upgradeOpt]++;
+        }
+
+        private IEnumerator ResetUpgrading()
+        {
+            yield return new WaitForSeconds(0.5f);
+            upgrading = false;
+        }
+
+        private void BuyUpgrades()
+        {
+            if (!Input.GetKeyDown(KeyCode.B) || cash < buyCost || !UpgradeManager.Instance.UpgradeComplete)
             {
                 return;
             }
@@ -95,7 +177,7 @@ namespace MoneyAirways
 
         private void RefreshUpgrade()
         {
-            if (cash < refreshCost || UpgradeManager.Instance.UpgradeComplete)
+            if (!Input.GetKeyDown(KeyCode.R) || cash < refreshCost || UpgradeManager.Instance.UpgradeComplete)
             {
                 return;
             }
@@ -106,10 +188,12 @@ namespace MoneyAirways
 
         public const int prize = 2;
         public const int refreshCost = 5;
-        public const int buyCost = 10;
+        public const int buyCost = 20;
+        public List<int> buyCosts = new List<int> { 0, 30, 40, 30, 30, 40, 30, 40, 50, 50 };
         public int cash = 0;
         private TMP_Text cashDisplay;
         private GameObject cashDisplayObj;
+        private bool upgrading = false;
     }
 
     [HarmonyPatch(typeof(GameDataWhiteBoard), "OnAircraftHandOff", new Type[] {})]
--- Readme.md
+++ Readme.md
@@ -1,8 +1,15 @@
 # Money Airways
 
-A Mini Airways mod allows you to purchase upgrades and refresh upgrade options actively. Press `B` to buy an upgrade (-$10) and press `R` to refresh upgrade list (-$5). Each hand-over/landing gets you two times the point worth of cash. Current cash is displayed on the top-left.
+A Mini Airways mod allows you to purchase upgrades and refresh upgrade options actively. Press `B` to buy an upgrade (-$20) and press `R` to refresh upgrade list (-$5). Each hand-over/landing gets you two times the point worth of cash. Current cash is displayed on the top-left.
 
 <img src="ui.png" width=50% height=50%>
+
+Press `Left SHIFT` + numbers `1` ~ `9` to purchase upgrade directly, 
+
+| `LShift`+`1`     | `LShift`+`2` | `LShift`+`3`   | `LShift`+`4` | `LShift`+`5`     | `LShift`+`6` | `LShift`+`7`  | `LShift`+`8`   | `LShift`+`9`   |
+|------------------|--------------|----------------|--------------|------------------|--------------|---------------|----------------|----------------|
+|   Waiting Area   |   Exit       |   Turn Speed   |   Airspace   |   Compensation   |   Waypoint   |   Holding WP  |   Landing WP   |   Takeoff WP   |
+|   $30            |   $40        |   $30          |   $30        |   $30            |   $30        |   $40         |   $50          |   $50          |
 
 # How to Install
 
@@ -16,6 +23,13 @@ A Mini Airways mod allows you to purchase upgrades and refresh upgrade options a
 一个能让你购买升级的迷你空管Mod。按`B`购买升级（-$10），按`R`刷新升级列表（-$5）。每次起飞降落可以获得2倍分数的钱。左上角可以看到当前现金。
 
 <img src="ui.png" width=50% height=50%>
+
+可以用 `Left SHIFT` + 数字键`1` ~ `9`直接购买升级
+
+| `LShift`+`1`     | `LShift`+`2` | `LShift`+`3`   | `LShift`+`4` | `LShift`+`5`     | `LShift`+`6` | `LShift`+`7`  | `LShift`+`8`   | `LShift`+`9`   |
+|----------|----------|----------|----------|----------|----------|----------|----------|----------|
+| 等待区   | 出口     | 转弯速度 | 空域     | 赔偿     | 航点     | 盘旋航点 | 降落航点 | 起飞航点 |
+| $30      | $40      | $30      | $30      | $30      | $30      | $40      | $50      | $50      |
 
 # 如何安装
 
